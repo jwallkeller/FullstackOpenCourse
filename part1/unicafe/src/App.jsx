3e5bd377db28a5568ee6075f9a95c1e5b37069fc
Jack Keller <jwallkeller@gmail.com>
2025-05-06 11:50:32 -0400

    Exercise 1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,16 +6,41 @@ const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 const Display = (props) => <p>{props.text} {props.stat}</p>
 
+const Percentage = (props) => <p>{props.text} {props.stat * 100}%</p>
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [avg, setAverage] = useState(0)
+  const [pos, setPositive] = useState(0)
 
-  const handleGoodClick = () => setGood(good+1)
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    const updatedTotal = total + 1
+    setGood(good + 1)
+    setTotal(updatedGood + neutral + bad)
+    setAverage((updatedGood - bad) / updatedTotal)
+    setPositive(updatedGood / updatedTotal)
+  }
   
-  const handleNeutralClick = () => setNeutral(neutral+1)
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    const updatedTotal = total + 1
+    setNeutral(neutral + 1)
+    setTotal(good + updatedNeutral + bad)
+    setPositive(good / updatedTotal)
+  }
   
-  const handleBadClick = () => setBad(bad+1)
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    const updatedTotal = total + 1
+    setBad(bad + 1)
+    setTotal(good + neutral + updatedBad)
+    setAverage((good - updatedBad) / updatedTotal)
+    setPositive(good / updatedTotal)
+  }
 
   return (
     <div>
@@ -27,6 +52,9 @@ const App = () => {
       <Display text='good' stat={good} />
       <Display text='neutral' stat={neutral} />
       <Display text='bad' stat={bad} />
+      <Display text='all' stat={total} />
+      <Display text='average' stat={avg} />
+      <Percentage text='positive' stat={pos} />
     </div>
   )
 }
