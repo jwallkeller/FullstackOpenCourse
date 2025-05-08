0d89577e7e980885e18ce2500e0bb4d33f936507
Jack Keller <jwallkeller@gmail.com>
2025-05-08 08:20:44 -0400

    Exercise 2.11: The phonebook step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ( {searchTerm, handleSearchTermChange} ) => {
   return (
@@ -35,16 +36,20 @@ const Persons = ( {displayedContacts} ) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
   const [displayedContacts, setDisplayedContacts] = useState(persons)
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+        setDisplayedContacts(response.data)
+      })
+  }, [])
 
   const addContact = (event) => {
     event.preventDefault()
